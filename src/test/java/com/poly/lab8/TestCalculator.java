148386050ea4cfea6a4c509567b6e7a1cf3433f9
DucHien1712 <tranduchien509@gmail.com>
2025-04-14 01:44:14 +0700

    test fail

---
+++ src/test/java/com/poly/lab8/TestCalculator.java
@@ -0,0 +1,19 @@
+package com.poly.lab8;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testSum() {
+        Calculator caculator = new Calculator();
+        Assert.assertEquals(caculator.sum(2, 3), 5);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator caculator = new Calculator();
+        Assert.assertEquals(caculator.sub(2, 2), 0);
+    }
+}
