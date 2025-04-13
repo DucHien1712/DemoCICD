5941d630f6381daacf2820cfd67afd4a0bec4d38
DucHien1712 <tranduchien509@gmail.com>
2025-04-14 01:27:06 +0700

    first commit

---
+++ src/main/java/com/poly/lab8/Calculator.java
@@ -0,0 +1,12 @@
+package com.poly.lab8;
+
+public class Calculator implements Icalculator {
+
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/poly/lab8/Icalculator.java
@@ -0,0 +1,7 @@
+package com.poly.lab8;
+
+public interface Icalculator {
+    Integer sum(Integer a, Integer b);
+
+    Integer sub(Integer a, Integer b);
+}
