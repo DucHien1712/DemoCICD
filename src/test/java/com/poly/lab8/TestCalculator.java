f7f1318be33b7bd568770bd57df596f2fa729beb
DucHien1712 <tranduchien509@gmail.com>
2025-04-14 10:11:56 +0700

    test wrong

--- src/test/java/com/poly/lab8/TestCalculator.java
+++ src/test/java/com/poly/lab8/TestCalculator.java
@@ -1,19 +1,20 @@
 package com.poly.lab8;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
 
     @Test
     public void testSum() {
         Calculator calculator = new Calculator();
-        Assert.assertEquals(calculator.sum(2, 3), 5);
+        assertEquals(calculator.sum(2, 3), 5);
     }
 
     @Test
     public void testSub() {
         Calculator calculator = new Calculator();
-        Assert.assertEquals(calculator.sub(5, 2), 0);
+        assertEquals(calculator.sub(5, 2), 3);
     }
 }
